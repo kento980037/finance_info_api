e9632e06396c5defa857ff0edf8386c4a3df5544
kento980037 <kento_0037@yahoo.co.jp>
2022-04-23 19:00:36 +0900

    add year_high rooting

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"finance-info-api/controller/kabutan"
 )
 
 func main() {
+
 	// make instance
 	e := echo.New()
 
@@ -17,9 +20,11 @@ func main() {
 
 	// config route
 	e.GET("/", hello)
+	e.GET("/year_high", kabutan.GetEntries())
 
 	// start the server on port number 1323
 	e.Logger.Fatal(e.Start(":1323"))
+
 }
 
 // define handler
